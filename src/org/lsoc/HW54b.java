399c9a6eee36841e45b4e2013fa40542476e00f9
darinakoe <156979980+darinakoe@users.noreply.github.com>
2025-09-04 17:33:37 +0000

    wrote 54b code

---
+++ src/org/lsoc/HW54b.java
@@ -0,0 +1,14 @@
+public class HW54b {
+  public static void main(String[] args) {
+    int num1 = 475;
+    int num2 = 821;
+    int num3 = 369;
+    int num4 = 562;
+
+    int sum = num1 + num2 + num3 + num4;
+    int ave = sum / 4;
+
+    System.out.println(sum);
+    System.out.println(ave);
+  }
+}
