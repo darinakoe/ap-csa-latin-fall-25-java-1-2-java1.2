0cb828fd3f4d78311f33d9a658c615c33fa73995
darinakoe <156979980+darinakoe@users.noreply.github.com>
2025-09-04 17:22:10 +0000

    wrote 54c code

---
+++ src/org/lsoc/HW54c.java
@@ -0,0 +1,11 @@
+public class HW54c {
+  public static void main (String[] args) {
+    double pi = 3.14159;
+    double radius = 3.712;
+    double area =  pi * radius * radius;
+    double circ = 2 * pi * radius;
+    System.out.println(radius);
+    System.out.println(circ);
+    System.out.println(area);
+  }
+}
